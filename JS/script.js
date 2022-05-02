cf4248fef90ce3a7b625dd0cc1904e48564f0bf2
themitope <ogunleyeoluwatosin2014@gmail.com>
2022-05-02 12:38:21 +0100

    contact us backend added

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	$("#submit").click(function(e){
+		e.preventDefault();
+        // alert('yesss');
+		$.ajax({
+			url: "backend/scripts/contact_us.php",
+			method: "POST",
+			data: $("#contact_form").serialize(),
+			beforeSend: function(){
+				$("#submit").val("Sending...");
+				$("#submit").attr("disabled", true);
+			},
+			success: function(data){
+				if(data == "success"){
+					// toastr.success('Your message has been sent successfully', 'Congratulations',{timeOut: 3000});
+					alert('Your message has been sent successfully');
+					setTimeout(function(){
+						window.location.href = "contact"
+					}, 3000);
+				}else{
+					// toastr.error("Error! "+ data);
+					alert("Error! "+ data);
+				}
+				$("#submit").val("Send Message");
+				$("#submit").attr("disabled", false);
+			}
+		})
+	});
+});
